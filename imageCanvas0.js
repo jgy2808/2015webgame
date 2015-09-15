f9878bba8035fa6c90789ca79c9f52748a49985c
jgy2808 <jgy2808@naver.com>
2015-09-15 20:58:27 +0900

    start Canvas

---
+++ imageCanvas0.js
@@ -0,0 +1,9 @@
+﻿window.addEventListener("load",drawScreen,true);
+
+function drawScreen()
+{
+var theCanvas = document.getElementById("GameCanvas");
+var Context = theCanvas.getContext("2d");
+Context.fillStyle = "#3498db";
+Context.fillRect(0,0,1000,700);
+}
---
+++ text.js
@@ -0,0 +1,41 @@
+﻿
+function Write99dan (_start, _end)
+{
+var start = _start;
+var end = _end;
+for (var i = start; i < end + 1; i++)
+{
+document.writeln();
+document.writeln(i + "단");
+for ( var j = 1; j < 10; j++)
+{
+document.writeln(i + "*" + j + " = " + i*j);
+}
+}
+}
+
+Write99dan(2,14);
+
+var arrayInt = new Array();
+arrayInt.push(3);
+arrayInt.push(36);
+arrayInt.push(369);
+document.writeln();
+for (i = 0; i<arrayInt.length; i++)
+{
+document.writeln(i+"번 인덱스의 원소값 : "+arrayInt[i]);
+}
+document.writeln("arrayInt 배열의 원소 수 : "+arrayInt.length);
+
+function Animal(name)
+{
+this.name = name;
+}
+Animal.prototype.Talk = function()
+{
+document.writeln("내 이름은 " + this.name + "야~");
+}
+var monkey = new Animal("원숭이");
+var bird = new Animal("새");
+monkey.Talk();
+bird.Talk();
