045111db7b74df2df95295fc8827801f388ed11a
jgy2808 <jgy2808@naver.com>
2015-09-18 08:57:12 +0900

    키보드입력 and 마우스입력

---
+++ Back_Player.js
@@ -0,0 +1,104 @@
+﻿var bMouseClicked = false;
+var intMouseX = 480;
+var intMouseY = 300;
+var strMouseStatus = "준비중";
+
+
+var imgBackground = new Image();
+imgBackground.src = "background.png";
+
+
+
+var imgPlayer = new Image();
+imgPlayer.src = "size_spongebob.png";
+
+imgPlayer.addEventListener("load", drawScreen, false);
+
+function drawScreen(){
+	var theCanvas = document.getElementById("GameCanvas");
+	var Context = theCanvas.getContext("2d");
+	Context.drawImage(imgBackground,0,0,1000,700);
+	Context.drawImage(imgPlayer,350,250,80,100);
+}
+
+window.addEventListener("load",drawScreen,false);
+window.addEventListener("keydown",onkeydown,false);
+window.addEventListener("keyup",onkeyup,false);
+window.addEventListener("mousemove",onMouseMove,false);
+window.addEventListener("mousedown",onMouseDown,false);
+window.addEventListener("mouseup",onMouseUp,false);
+
+var imgPlayer = new Image();
+imgPlayer.src = "size_spongebob.png";
+imgPlayer.addEventListener("load",drawScreen,false);
+
+var strKeyEventType = "None";
+var strKeyEventValue = "None";
+
+function drawScreen(){
+	var theCanvas = document.getElementById("GameCanvas");
+	var Context = theCanvas.getContext("2d");
+	Context.drawImage(imgBackground,0,0,1000,700);
+	Context.drawImage(imgPlayer,350,250,50,55);
+	/*Context.Style = "#00f";
+	Context.font = '24px nanumgothic';
+	Context.textBaseline = "top";
+	Context.fillText("입력된 키는 :" + strKeyEventValue,5,5);
+	Context.fillText("키 입력 상태는 : " + strKeyEventType,5,30);*/
+	/*Context.fillStyle = "#ff0";
+	Context.fillRect(0,0,1074,768);*/
+	Context.drawImage(imgBackground,0,0,1000,700);
+	Context.drawImage(imgPlayer, intMouseX, intMouseY);
+	Context.fillStyle = "#000000";
+	Context.font = '24px Arial';
+	Context.textBaseline = "top";
+	Context.fillText("발생한 마우스 이벤트는 :" + strMouseStatus,5,5);
+	Context.fillText("마우스 좌표는 : " + "X = " +intMouseX + " Y = "+ intMouseY,5,30);
+
+
+}
+
+function onMouseDown(e){
+	strMouseStatus = "클릭!";
+	var theCanvas = document.getElementById("GameCanvas");
+	bMouseClicked = true;
+	intMouseX = e.clientX - theCanvas.offsetLeft-42;
+	intMouseY = e.clientY - theCanvas.offsetTop-50;
+	drawScreen();
+}
+
+
+function onMouseMove(e){
+	strMouseStatus = "Moving now";
+	if (bMouseClicked) {
+		var theCanvas = document.getElementById("GameCanvas");
+		bMouseClicked = true;
+		intMouseX = e.clientX - theCanvas.offsetLeft-42;
+		intMouseY = e.clientY - theCanvas.offsetTop-50;
+		drawScreen();
+	}
+}
+
+function onMouseUp(e) {
+	strMouseStatus = "클릭 끝!";
+	bMouseClicked = false;
+	intMouseX = 480;
+	intMouseY = 300;
+	drawScreen();
+}
+
+/*function onkeydown(e)
+{
+	strKeyEventType = e.type;
+	if(e.keyCode)code = e.keyCode;
+	strKeyEventValue = code;
+	drawScreen();
+}
+
+function onkeyup(e)
+{
+	strKeyEventType = e.type;
+	if(e.keyCode)code = e.keyCode;
+	strKeyEventValue = String.fromCharCode(code);
+	drawScreen();
+}*/
--- imageCanvas0.js
+++ imageCanvas0.js
@@ -6,4 +6,45 @@ var theCanvas = document.getElementById("GameCanvas");
 var Context = theCanvas.getContext("2d");
 Context.fillStyle = "#3498db";
 Context.fillRect(0,0,1000,700);
+/*Context.beginPath();
+Context.moveTo(10,10);
+Context.lineTo(150,100);
+Context.strokeStyle = "#000";
+Context.stroke();
+
+Context.beginPath();
+Context.arc(200,60,50,1*Math.PI,2*Math.PI,false);
+Context.fillStyle="#ff0";
+Context.fill();
+
+var gradient = Context.createLinearGradient(0,0,170,0);
+gradient.addColorStop("0", "white");
+gradient.addColorStop("0.5","white");
+gradient.addColorStop("1","white");
+
+Context.strokeStyle = gradient;
+Context.lineWidth = 8;
+Context.strokeRect(150,150,500,300);
+Context.fillStyle = "#fff";
+Context.fillRect(150,150,500,300);
+
+Context.beginPath();
+Context.arc(400,290,80,5*Math.PI,6*Math.PI,false);
+Context.fillStyle = "#ff0000";
+Context.fill();
+Context.beginPath();
+Context.arc(400,290,80,5*Math.PI,6*Math.PI,true);
+Context.fillStyle = "#0000ff";
+Context.fill();
+
+Context.beginPath();
+Context.fillStyle = "#000";
+Context.fillRect(160,180,140,30);
+Context.fill();
+
+Context.fillStyle = "#000";
+Context.fillRect(160,220,140,30);
+
+Context.fillStyle = "#000";
+Context.fillRect(160,260,140,30);*/
 }
